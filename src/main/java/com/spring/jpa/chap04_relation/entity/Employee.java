4acc093a0df519614b48fc658da852432ef3d78a
teataeho <rlaxogh9504@gmail.com>
2023-06-19 12:24:30 +0900

    2023.06.19 #4. JPA 연관관계 매핑(단방향, 양방향), FETCH JOIN(N+1)

---
+++ src/main/java/com/spring/jpa/chap04_relation/entity/Employee.java
@@ -0,0 +1,37 @@
+package com.spring.jpa.chap04_relation.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Setter @Getter
+// jpa 연관관계 매핑에서 연관관계 데이터는 ToString에서 제외해야 한다.
+@ToString(exclude = {"department"})
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tbl_emp")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "emp_id")
+    private long id;
+
+    @Column(name = "emp_name", nullable = false)
+    private String name;
+
+    // EAGER: 항상 무조건 조인을 수행
+    // LAZY: 필요한 경우에만 조인을 수행 (실무)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "dept_id")
+    private Department department;
+
+    public void setDepartment(Department department) {
+        this.department = department;
+        department.getEmployees().add(this);
+    }
+
+}
---
+++ src/test/java/com/spring/jpa/chap04_relation/repository/DepartmentRepositoryTest.java
@@ -0,0 +1,121 @@
+package com.spring.jpa.chap04_relation.repository;
+
+import com.spring.jpa.chap04_relation.entity.Department;
+import com.spring.jpa.chap04_relation.entity.Employee;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class DepartmentRepositoryTest {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @Autowired
+    DepartmentRepository departmentRepository;
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Test
+    @DisplayName("특정 부서를 조회하면 해당 부서원들도 함께 조회되어야 한다.")
+    void testFindDept() {
+        //given
+        long id = 2L;
+        //when
+        Department department = departmentRepository.findById(id)
+                .orElseThrow();
+        //then
+        System.out.println("\n\n\n");
+        System.out.println("department = " + department);
+        System.out.println("department.getEmployees() = " + department.getEmployees());
+        System.out.println("\n\n\n");
+
+    }
+
+    @Test
+    @DisplayName("Lazy 로딩과 Eager 로딩의 차이")
+    void testLazyAndEager() {
+        // 3번 사원을 조회하고 싶은데, 굳이 부서 정보는 필요없다.
+        //given
+        long id = 3L;
+        //when
+        Employee employee = employeeRepository.findById(id).orElseThrow();
+        //then
+        System.out.println("\n\n\n");
+        System.out.println("employee = " + employee.getDepartment());
+        System.out.println("\n\n\n");
+    }
+    
+    @Test
+    @DisplayName("양방향 연관관계에서 연관데이터의수정")
+    void testChangeDept() {
+        // 3번 사원의 부서를 2번 부서에서 1번 부서로 변경하도록 한다.
+        //given
+        long id = 3L;
+        //when
+        Employee foundEmp = employeeRepository.findById(id).orElseThrow();
+        Department newDept = departmentRepository.findById(1L).orElseThrow();
+
+        foundEmp.setDepartment(newDept);
+
+        //insert, update, delete같은 경우 JPA는 데이터베이스에 트랜젝션 이후에 마지막으로 save를 진행한다.
+        employeeRepository.save(foundEmp);
+
+        // 변경 감지(더티 체크) 후 변경된 내용을 DB에 반영하는 역할을 합니다.
+//        entityManager.flush();
+//        entityManager.clear();
+
+        //then
+        //1번 부서 정보를 조회해서 모든 사원을 보겠다.
+        Department foundDept = departmentRepository.findById(1L).orElseThrow();
+
+        System.out.println("\n\n\n");
+        foundDept.getEmployees().forEach(System.out::println);
+        System.out.println("\n\n\n");
+    }
+
+    @Test
+    @DisplayName("N + 1 문제 발생 예시")
+    void testNPlus1Ex() {
+        //given
+        List<Department> departments = departmentRepository.findAll();
+        //when
+        departments.forEach(dept -> {
+            System.out.println("\n\n======= 사원 리스트 =======");
+
+            List<Employee> employees = dept.getEmployees();
+            System.out.println("employees = " + employees);
+
+            System.out.println("\n\n");
+        });
+        //then
+    }
+
+    @Test
+    @DisplayName("N + 1 문제 해결 예시")
+    void testNPlus1Solution() {
+        //given
+        List<Department> departments = departmentRepository.findAllIncludeEmployees();
+        //when
+        departments.forEach(dept -> {
+            System.out.println("\n\n======= 사원 리스트 =======");
+
+            List<Employee> employees = dept.getEmployees();
+            System.out.println("employees = " + employees);
+
+            System.out.println("\n\n");
+        });
+        //then
+    }
+
+}
