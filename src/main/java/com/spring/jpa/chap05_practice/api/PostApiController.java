38840f9d49421bb5ab42672f2b39b17e3a9574d3
teataeho <rlaxogh9504@gmail.com>
2023-06-19 16:18:48 +0900

    2023.06.19 #6. 종합 연습예제 작성 중(posts -> GET)

--- src/main/java/com/spring/jpa/chap05_practice/api/PostApiController.java
+++ src/main/java/com/spring/jpa/chap05_practice/api/PostApiController.java
@@ -1,8 +1,12 @@
 package com.spring.jpa.chap05_practice.api;
 
+import com.spring.jpa.chap05_practice.dto.PageDTO;
+import com.spring.jpa.chap05_practice.dto.PostListResponseDTO;
 import com.spring.jpa.chap05_practice.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +26,12 @@ public class PostApiController {
      */
 
     private final PostService postService;
+
+    @GetMapping
+    public ResponseEntity<?> list(PageDTO pageDTO) { //?로 설정하면 알맞는 엔터티를 찾아서 리턴한다.
+        log.info("/api/v1/posts?page={}&size={}", pageDTO.getPage(), pageDTO.getSize());
+        PostListResponseDTO dto = postService.getPosts(pageDTO);
+        return null;
+    }
 
 }
--- src/main/java/com/spring/jpa/chap05_practice/entity/Post.java
+++ src/main/java/com/spring/jpa/chap05_practice/entity/Post.java
@@ -26,6 +26,8 @@ public class Post {
 
     @Column(nullable = false)
     private String writer; //작성자
+    @Column(nullable = false)
+    private String title; //제목
 
     private  String content; //내용
 
